3df58bdd1a3f3eda36bc9f919eac14b1b6d46ed5
Zhou Xiong <zhouxiong@foreveross.com>
2019-11-08 14:16:46 +0800

    for和when测试

--- app/src/main/java/com/slin/study/kotlin/test/grammer/Expr.kt
+++ app/src/main/java/com/slin/study/kotlin/test/grammer/Expr.kt
@@ -1,19 +1,58 @@
 package com.slin.study.kotlin.test.grammer
 
-interface Expr
-class Num(val value: Int): Expr
-class Sum(val left: Int, val right:Int):Expr
+import kotlin.random.Random
 
-fun eval(e: Expr): Int{
-    if (e is Num){
+interface Expr
+class Num(val value: Int) : Expr
+class Sum(val left: Int, val right: Int) : Expr
+
+fun main() {
+    println(eval(Sum(eval(Sum(1, 2)), eval(Num(3)))))
+    forTest()
+}
+
+fun eval(e: Expr): Int {
+    if (e is Num) {
         return e.value
-    } else if(e is Sum) {
+    } else if (e is Sum) {
         return e.left + e.right
-    } else{
+    } else {
         throw IllegalArgumentException("Unknown expression")
     }
 }
 
-fun main() {
-//    println(eval(eval(Num(1), Num(2)))
+fun eval2(e: Expr) =
+    when (e) {
+        is Num -> e.value
+        is Sum -> e.left + e.right
+        else -> throw java.lang.IllegalArgumentException("Unknown expression")
+    }
+
+fun forTest() {
+    val testArray: ArrayList<Int> = ArrayList(10)
+    //初始化一个数列
+    for (i in 0 until 10) {
+        testArray.add(Random.nextInt(0, 10))
+    }
+    println("init data: $testArray")
+
+    //冒泡排序算法
+    for ((index, _) in testArray.withIndex()) {
+        for (j in 0 until testArray.size - 1 - index) {
+            if (testArray[j] < testArray[j + 1]) {
+                val temp = testArray[j]
+                testArray[j] = testArray[j + 1]
+                testArray[j + 1] = temp
+            }
+        }
+    }
+
+    println("sort data: $testArray")
+
+    for (i in testArray.size-1 downTo 0 step 1){
+        print(testArray[i])
+        print(" ")
+    }
+
 }
+

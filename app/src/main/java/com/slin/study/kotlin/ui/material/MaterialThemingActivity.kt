e778c956f8b1971e8272b126611f51ba40a0ad25
slin_huibo <dxslin@qq.com>
2020-07-31 17:19:04 +0800

    material dialog

--- app/src/main/java/com/slin/study/kotlin/ui/material/MaterialThemingActivity.kt
+++ app/src/main/java/com/slin/study/kotlin/ui/material/MaterialThemingActivity.kt
@@ -2,10 +2,12 @@ package com.slin.study.kotlin.ui.material
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.CompoundButton
+import android.widget.TextView
+import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import com.google.android.material.chip.Chip
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.slin.study.kotlin.R
 import com.slin.study.kotlin.base.BaseActivity
 import com.slin.study.kotlin.util.toast
@@ -17,6 +19,7 @@ class MaterialThemingActivity : BaseActivity() {
 
     private val TAG: String? = MaterialThemingActivity::class.simpleName
 
+    private val dialogItemFruits = arrayOf("苹果", "梨子", "西瓜", "桃子")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,7 +35,7 @@ class MaterialThemingActivity : BaseActivity() {
      * Button click listener
      */
     fun buttonClick(v: View) {
-        if (v is Button) {
+        if (v is TextView) {
             toast("Click: ${v.text}")
         }
     }
@@ -101,6 +104,81 @@ class MaterialThemingActivity : BaseActivity() {
                 til_custom_input.isErrorEnabled = false;
             }
         })
+    }
+
+    fun showAlertDialog(v: View) {
+        MaterialAlertDialogBuilder(this)
+            .setTitle("消息提示")
+            .setMessage("这是消息提示")
+            .setPositiveButton("确定") { _, _ ->
+                Toast.makeText(this, "点击确定", Toast.LENGTH_SHORT).show()
+            }
+            .setNegativeButton("取消") { _, _ ->
+                Toast.makeText(this, "点击取消", Toast.LENGTH_SHORT).show()
+            }
+            .show()
+    }
+
+    fun showSimpleDialog(v: View) {
+        MaterialAlertDialogBuilder(this)
+            .setTitle("选择水果")
+            .setItems(dialogItemFruits) { _, which ->
+                Toast.makeText(this, "选择了${dialogItemFruits[which]}", Toast.LENGTH_SHORT).show()
+            }
+            .show()
+    }
+
+    fun showSingleChoiceDialog(v: View) {
+        MaterialAlertDialogBuilder(this)
+            .setTitle("选择水果")
+            .setSingleChoiceItems(dialogItemFruits, -1) { _, which ->
+                Toast.makeText(this, "选择了${dialogItemFruits[which]}", Toast.LENGTH_SHORT).show()
+            }
+            .setPositiveButton("确定") { _, _ ->
+                Toast.makeText(this, "点击确定", Toast.LENGTH_SHORT).show()
+            }
+            .setNegativeButton("取消") { _, _ ->
+                Toast.makeText(this, "点击取消", Toast.LENGTH_SHORT).show()
+            }
+            .show()
+
+    }
+
+    fun showMultiChoiceDialog(v: View) {
+        val checkItems = BooleanArray(dialogItemFruits.size) { false }
+        MaterialAlertDialogBuilder(this)
+            .setTitle("选择水果")
+            .setMultiChoiceItems(dialogItemFruits, checkItems) { _, which, isChecked ->
+                Toast.makeText(
+                    this,
+                    "${if (isChecked) "选择了" else "取消选择了"}${dialogItemFruits[which]}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            .setPositiveButton("确定") { _, _ ->
+                Toast.makeText(this, "点击确定", Toast.LENGTH_SHORT).show()
+            }
+            .setNegativeButton("取消") { _, _ ->
+                Toast.makeText(this, "点击取消", Toast.LENGTH_SHORT).show()
+            }
+            .show()
+    }
+
+    fun showAllButtonDialog(v: View) {
+        MaterialAlertDialogBuilder(this)
+            .setTitle("All Button Dialog")
+            .setMessage("this is all button dialog")
+            .setPositiveButton("ok") { _, _ ->
+                Toast.makeText(this, "Click Ok", Toast.LENGTH_SHORT).show()
+            }
+            .setNegativeButton("Cancel") { _, _ ->
+                Toast.makeText(this, "Click Cancel", Toast.LENGTH_SHORT).show()
+            }
+            .setNeutralButton("Neutral") { _, _ ->
+                Toast.makeText(this, "Click Neutral", Toast.LENGTH_SHORT).show()
+            }
+            .setIcon(R.drawable.cartoon_2)
+            .show()
     }
 
 }

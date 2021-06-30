11d16140e6e1f4d39a143563d0bb4e310417a370
slin_huibo <dxslin@qq.com>
2021-06-30 16:20:09 +0800

    [dev] SystemClock time

--- study/src/main/java/com/slin/study/kotlin/ui/theory/HandlerTestActivity.kt
+++ study/src/main/java/com/slin/study/kotlin/ui/theory/HandlerTestActivity.kt
@@ -3,6 +3,7 @@ package com.slin.study.kotlin.ui.theory
 import android.os.Bundle
 import android.os.Handler
 import android.os.HandlerThread
+import android.os.SystemClock
 import com.slin.study.kotlin.base.BaseActivity
 import com.slin.study.kotlin.databinding.ActivityHandlerTestBinding
 import com.slin.study.kotlin.util.Logger
@@ -49,10 +50,27 @@ class HandlerTestActivity : BaseActivity() {
                 Logger.log(TAG, "post delay message ")
                 handler.postDelayed({
                     Logger.log(TAG, "post delay message execute")
-                }, 3000)
+                }, 60_000)
             }
             btnAddIdleHandler.setOnClickListener {
                 addIdleHandler()
+            }
+            btnGetTime.setOnClickListener {
+                // 系统时间，修改系统设置时，时间可能会被修改
+                Logger.log(TAG, "System.currentTimeMillis: \t\t\t\t${System.currentTimeMillis()}")
+                // 系统启动之后的运行时间，深度睡眠不计入
+                Logger.log(TAG, "SystemClock.uptimeMillis: \t\t\t\t${SystemClock.uptimeMillis()}")
+                // 系统启动之后的运行时间，包括深度睡眠时
+                Logger.log(
+                    TAG,
+                    "SystemClock.elapsedRealtime: \t\t\t${SystemClock.elapsedRealtime()}"
+                )
+                // 当前线程运行时间
+                Logger.log(
+                    TAG,
+                    "SystemClock.currentThreadTimeMillis: \t${SystemClock.currentThreadTimeMillis()}"
+                )
+
             }
         }
 
